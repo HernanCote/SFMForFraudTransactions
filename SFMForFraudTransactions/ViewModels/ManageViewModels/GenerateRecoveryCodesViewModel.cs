ff5316ba4c531060966c59d06df827a67ff178d3
Hernán Cote <hernan.cote@outlook.com>
2018-04-04 14:31:15 -0500

    Initial commit

---
+++ SFMForFraudTransactions/ViewModels/ManageViewModels/GenerateRecoveryCodesViewModel.cs
@@ -0,0 +1,7 @@
+﻿namespace SFMForFraudTransactions.ViewModels.ManageViewModels
+{
+    public class GenerateRecoveryCodesViewModel
+    {
+        public string[] RecoveryCodes { get; set; }
+    }
+}
